ca724fae611475113283d854854f534ad289cf11
IcePlayer <nathanchevalet@orange.fr>
2024-03-01 11:20:35 +0100

    update frontend

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,32 +6,53 @@ const longUrlInput = document.querySelector('#long-url')
 const labelLongUrlInput = document.querySelector('.label[for="short-url"]')
 const shortUrlInput = document.querySelector('#short-url')
 const customId = document.querySelector('#custom-id')
+const shortLink = document.querySelector('.short-link')
 
 form.onsubmit = reduceLink
 
 // helperError.style.display = 'block'
 
+longUrlInput.value = ''
+shortUrlInput.value = ''
+
 switchInputId()
 function switchInputId() {
+  helperError.style = 'none'
   shortUrlInput.disabled = !customId.checked
   shortUrlInput.required = customId.checked
   const classAction = !customId.checked ? 'add' : 'remove'
   labelLongUrlInput.classList[classAction]('disabled')
 }
 
-function reduceLink(e) {
+async function reduceLink(e) {
   e.preventDefault()
+  helperError.style = 'none'
+  helperSuccess.style = 'none'
   try {
-    fetch('http://localhost:4000/api', {
+    const response = await fetch('http://localhost:4000/api', {
       method: 'POST',
       body: JSON.stringify({
         Url: longUrlInput.value,
         ShortUrl: shortUrlInput.value
       })
-    })
-      .then(response => response.text()) // Read response as text
-      .then(data => alert(data)); // Alert the response
-  } catch {
-    helperError.style.display = 'block'
+    });
+
+    if (response.status === 500) {
+      throw new Error(response)
+    }
+    const data = await response.json()
+    console.log(response)
+    console.log(data)
+
+    shortUrlInput.value = ''
+    longUrlInput.value = ''
+    helperSuccess.style.display = 'block'
+    console.log(data)
+    const shortLinkValue =  `http://localhost:4000/api/${data.shortUrl}`
+    shortLink.innerText = shortLinkValue
+    shortLink.href = shortLinkValue
+  } catch (error) {
+    console.log(error)
+    helperError.style.display = 'block' 
   }
 }
