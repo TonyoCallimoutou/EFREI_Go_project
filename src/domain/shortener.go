234d930480584b38ec8797b776b5b7961acfa26d
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2024-03-01 15:43:32 +0100

    fix type Shortener

--- src/domain/shortener.go
+++ src/domain/shortener.go
@@ -3,8 +3,9 @@ package domain
 import "time"
 
 type Shortener struct {
-	ID        string    `json:"id"`
-	Url       string    `json:"url"`
-	ShortUrl  string    `json:"shortUrl"`
-	ExpiredAt time.Time `json:"expired_at"`
+	ID        string    `json:"id" db:"id"`
+	Url       string    `json:"url" db:"url"`
+	ShortUrl  string    `json:"shortUrl" db:"shortUrl"`
+	ExpiredAt time.Time `json:"expired_at" db:"expiredAt"`
+	Count     int       `json:"count" db:"count"`
 }
